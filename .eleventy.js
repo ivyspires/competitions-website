31b5ec45bf44b7450e1ee2fb476f29d0b5a73fd7
ivyspires <anup@ivyspires.com>
2026-07-28 16:50:31 +0530

    Create .eleventy.js

---
+++ .eleventy.js
@@ -0,0 +1,17 @@
+module.exports = function (eleventyConfig) {
+  // Static assets: public/styles.css, public/script.js, public/downloads/*
+  // are copied to the output root so URLs like /styles.css, /downloads/x.pdf work.
+  eleventyConfig.addPassthroughCopy({ "public": "." });
+
+  return {
+    dir: {
+      input: "src",
+      includes: "_includes",
+      data: "_data",
+      output: "_site"
+    },
+    templateFormats: ["njk", "html"],
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk"
+  };
+};
